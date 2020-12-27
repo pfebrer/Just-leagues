624441174a46acb11ab9a656540c5a01c37c3802
Pol <pfebrer96@gmail.com>
2020-12-27 17:36:33 +0100

    Trying github secrets for api key

--- api/Firebase.js
+++ api/Firebase.js
@@ -22,7 +22,7 @@ class Firebase {
 
     //Here we initialize the app
     var firebaseConfig = {
-      apiKey: "AIzaSyAHqGQDf55s2jACqlyn3ejicyk9ESrU3KM",
+      apiKey: process.env.FIREBASE_API_KEY,//"AIzaSyAHqGQDf55s2jACqlyn3ejicyk9ESrU3KM",
       authDomain: "squash-leagues-94b68.firebaseapp.com",
       databaseURL: "https://squash-leagues-94b68.firebaseio.com",
       projectId: "squash-leagues-94b68",
